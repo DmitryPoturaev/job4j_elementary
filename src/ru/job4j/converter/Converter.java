ded4097ad862cf1cf7ee5b31bfe9281783eebe9e
DmitryPoturaev <poturaev_d@mail.ru>
2021-03-16 21:12:34 +0500

    1. Модульные тесты. [#494166]  - из метода main() исключен код для тестирования результатов, лобавленный на прошлом уроке;  - добавлен класс ConverterTest;  - добавлены тест-методы.

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -11,19 +11,15 @@ public class Converter {
 
     public static void main(String[] args) {
         int rubles = 190;
-        int expectEuro = 2;
-        int expectDollar = 3;
 
         String splitter = " - ";
 
         System.out.println(rubles + " rubles are:");
 
         int euro = Converter.rubleToEuro(rubles);
-        boolean passed = expectEuro == euro;
-        System.out.println(splitter + euro + " euro. Test result : " + passed);
+        System.out.println(splitter + euro + " euro.");
 
         int dollar = Converter.rubleToDollar(rubles);
-        passed = expectDollar == dollar;
-        System.out.println(splitter + dollar + " dollars. Test result : " + passed);
+        System.out.println(splitter + dollar + " dollars.");
     }
 }
