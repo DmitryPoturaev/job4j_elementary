be262f7be31e1bcd700e90f57f283ac85e677beb
DmitryPoturaev <poturaev_d@mail.ru>
2021-03-21 18:27:19 +0500

    5.5. Простое число [#494193]  - исправлен метод check;  - добавлен тест-метод для проверки числа 2.

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,14 +2,10 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
-        for (int i = 2; i < number - 1; i++) {
-            if (i == 2) {
-                prime = number % i != 0;
-            } else {
-                prime &= number % i != 0;
-            }
-            if (!prime) {
+        boolean prime = number > 1;
+        for (int i = 2; i < number -1; i++) {
+            if (number % i == 0) {
+                prime = false;
                 break;
             }
         }
