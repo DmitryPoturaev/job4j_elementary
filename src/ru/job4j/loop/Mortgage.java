5369d53e57b5df675ee45ec62435a9a7260a888f
DmitryPoturaev <poturaev_d@mail.ru>
2021-03-22 18:53:37 +0500

    5.7. Ипотека [#494192]  - оптимизация.

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,7 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double debt = amount + amount * (percent / 100);
-        while (debt > 0) {
-            debt -= salary;
-            debt += debt * (percent / 100);
+        while ((amount + amount * (percent / 100) - salary) > 0) {
             year++;
         }
         return year;
