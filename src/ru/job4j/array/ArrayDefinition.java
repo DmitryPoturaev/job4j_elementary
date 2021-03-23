9367b9fe425b64d476c9371a7998439c499f9469
DmitryPoturaev <poturaev_d@mail.ru>
2021-03-23 20:03:53 +0500

    6.0.2. Заполнение массива. [#494114]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -17,8 +17,9 @@ public class ArrayDefinition {
         names[2] = "Petr Petrov";
         names[3] = "Oleg Olegov";
 
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
-        }
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
